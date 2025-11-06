d4876e84d6c664e0feb167db662d1c3bf063d7db
NicolasMMI <nicolas.ginisty@gmail.com>
2025-11-06 08:51:38 +0100

    c est le debut

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    </head>
+   <body>
+        <h1>bonjour</h1>
+        <div>
+            <form action="connexion.php" method="get">
+                <input type="text" id="identifiant" name="Utilisateur" size="20" />
+                <input type="password" id="password" name="MotDePasse" size="20"/>
+                <input type="submit" value="Envoi"/>
+            </form>
+    <body>
+<html>
